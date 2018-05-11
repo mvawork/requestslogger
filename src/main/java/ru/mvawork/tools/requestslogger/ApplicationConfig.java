94d115522aba2e2684df29047930ee464662f539
menshevva <mvawork@mail.ru>
2018-05-11 16:11:25 +0300

    Начальный комит

---
+++ src/main/java/ru/mvawork/tools/requestslogger/ApplicationConfig.java
@@ -0,0 +1,28 @@
+package ru.mvawork.tools.requestslogger;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }
+
+    @Bean
+    public FilterRegistrationBean registration(HiddenHttpMethodFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean(filter);
+        registration.setEnabled(false);
+        return registration;
+    }
+
+}
---
+++ src/main/java/ru/mvawork/tools/requestslogger/RequestsLoggerApplication.java
@@ -0,0 +1,12 @@
+package ru.mvawork.tools.requestslogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestsLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestsLoggerApplication.class, args);
+    }
+}
